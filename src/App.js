7dc3ffeb74df3c6eedfc762c18bfe5a58f977d87
QuyenThao <thaoquyen23012003@gmail.com>
2021-07-31 07:22:04 +0700

    4 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from "./features/card/Card.js"
-import Slice from "./features/slice/Slice.js"
-import Menu from './features/menu/Menu.js'
+import Card from './features/card/Card.js';
+import Slice from './features/slice/Slice.js';
+import Menu from './features/menu/Menu.js';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Card />
       <Slice />
     </div>
